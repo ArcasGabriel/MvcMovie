b74870de2c41cca4bb7eb201bc56db8221462d7f
Arcaș Gabriel <38486343+ArcasGabriel@users.noreply.github.com>
2020-04-04 08:41:35 +0300

    Added a new view and the controller for it

--- Controllers/HelloWorldController.cs
+++ Controllers/HelloWorldController.cs
@@ -6,13 +6,16 @@ namespace MvcMovie.Controllers
     public class HelloWorldController : Controller
     {
         // GET /HelloWorld/
-        public string Index() {
-            return "This is my default action..";
+        public IActionResult Index()
+        {
+            return View();
         }
 
         // GET /HelloWorld/Welcome
-        public string Welcome(string name, int numTimes=1) {
-            return HtmlEncoder.Default.Encode($"Hello {name}, Numtimes is: {numTimes}");
+        public IActionResult Welcome(string name, int numTimes=1) {
+                       ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
         }
     }
 }
